4104fa295af1206bfde2ce877e2d9f3a3a4ac459
Neha Kumari <nehagupta3136@gmail.com>
2021-05-17 23:14:33 +0530

    Gradle Project

---
+++ src/test/java/Insert.java
@@ -0,0 +1,10 @@
+//public class insert {
+public class Insert {
+    public static void main(String args[]){
+        StringBuffer sb=new StringBuffer("Black");
+        sb.insert(1,"Green");//Insert the Green at the 1st index
+        System.out.println(sb);//prints PGreenink
+
+    }
+}
+
